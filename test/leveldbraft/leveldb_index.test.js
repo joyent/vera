baa30b801f7c31092b294d96f5ead95b5730341c
Nate Fitch <nate.fitch@joyent.com>
2013-12-10 14:03:17 -0800

    Testing reopening closed db.

---
+++ test/leveldbraft/leveldb_index.test.js
@@ -0,0 +1,95 @@
+// Copyright (c) 2013, Joyent, Inc. All rights reserved.
+
+var assert = require('assert-plus');
+var bunyan = require('bunyan');
+var fs = require('fs');
+var helper = require('../helper.js');
+var leveldbIndex = require('../../lib/leveldb');
+var path = require('path');
+var StateMachine = require('../memraft/statemachine');
+var stream = require('stream');
+var vasync = require('vasync');
+
+
+
+///--- Globals
+
+var test = helper.test;
+var LOG = bunyan.createLogger({
+    level: (process.env.LOG_LEVEL || 'fatal'),
+    name: 'leveldb_index-test',
+    stream: process.stdout
+});
+var TMP_DIR = path.resolve(path.dirname(__dirname), '..') + '/tmp';
+var DB_FILE = TMP_DIR + '/leveldb_index_test.db';
+var KEY = new Buffer('00', 'hex');
+var VALUE = { 'foo': 'bar' };
+
+
+
+///--- Tests
+
+test('create new log, close and open', function (t) {
+    vasync.pipeline({
+        'arg': {},
+        'funcs': [
+            function mkTmpDir(_, subcb) {
+                fs.mkdir(TMP_DIR, function (err) {
+                    if (err && err.code !== 'EEXIST') {
+                        return (subcb(err));
+                    }
+                    return (subcb());
+                });
+            },
+            function removeOldLevelDb(_, subcb) {
+                helper.rmrf(DB_FILE, subcb);
+            },
+            function createDb(_, subcb) {
+                leveldbIndex.createOrOpen({
+                    'log': LOG,
+                    'location': DB_FILE
+                }, function (err, res) {
+                    if (err) {
+                        subcb(err);
+                    }
+                    t.ok(res.created);
+                    _.db = res.db;
+                    subcb();
+                });
+            },
+            function putFirst(_, subcb) {
+                _.db.put(KEY, VALUE, subcb);
+            },
+            function close(_, subcb) {
+                _.db.close(subcb);
+            },
+            function openAgain(_, subcb) {
+                leveldbIndex.createOrOpen({
+                    'log': LOG,
+                    'location': DB_FILE
+                }, function (err, res) {
+                    if (err) {
+                        return (subcb(err));
+                    }
+                    t.ok(res.created === false);
+                    _.reopened = res.db;
+                    subcb();
+                });
+            },
+            function getAndCheckKey(_, subcb) {
+                _.reopened.get(KEY, function (err, value) {
+                    if (err) {
+                        return (subcb(err));
+                    }
+                    t.deepEqual(VALUE, value);
+                    subcb();
+                });
+            }
+        ]
+    }, function (err) {
+        if (err) {
+            t.fail(err);
+        }
+        t.done();
+    });
+});
