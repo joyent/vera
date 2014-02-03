5cf32bdf0d170a4f5f0e579b189768a8725ee2ef
Nate Fitch <nate.fitch@joyent.com>
2014-02-03 14:29:39 -0800

    LevelDb install snapshot.

--- test/leveldbraft/leveldb_log.test.js
+++ test/leveldbraft/leveldb_log.test.js
@@ -5,6 +5,8 @@ var bunyan = require('bunyan');
 var fs = require('fs');
 var helper = require('../helper.js');
 var LevelDbLog = require('../../lib/leveldb/log');
+var lib = require('../../lib/leveldb');
+var memStream = require('../../lib').memStream;
 var path = require('path');
 var StateMachine = require('../memraft/statemachine');
 var stream = require('stream');
@@ -20,6 +22,7 @@ var commandLogTests = require('../share/command_log_tests.js');
 
 var after = nodeunitPlus.after;
 var before = nodeunitPlus.before;
+var test = nodeunitPlus.test;
 var LOG = bunyan.createLogger({
     level: (process.env.LOG_LEVEL || 'fatal'),
     name: 'leveldb_log-test',
@@ -78,5 +81,124 @@ after(function (cb) {
         ]
     }, function (err) {
         cb(err);
+    });
+});
+
+
+
+///--- Level-db only tests
+
+test('verify lastLogIndex in db', function (t) {
+    var self = this;
+
+    function getAndCompareIndex(expected, cb) {
+        function onGet(err, v) {
+            if (err) {
+                return (cb(err));
+            }
+            t.equal(expected, v);
+            cb();
+        }
+        self.clog.db.get(lib.internalPropertyKey('lastLogIndex'),
+                         onGet);
+    }
+
+    var funcs = [
+        function (_, subcb) {
+            self.clog.append({
+                'commitIndex': 0,
+                'term': 0,
+                //No index, so append!
+                'entries': memStream([ { 'term': 2,
+                                         'command': 'command-1-2' } ])
+            }, function (err, entry) {
+                if (err) {
+                    subcb(err);
+                    return;
+                }
+                t.equal(1, entry.index);
+                t.deepEqual(self.clog.last(), entry);
+                t.equal(2, self.clog.nextIndex);
+                getAndCompareIndex(1, subcb);
+            });
+        },
+        function (_, subcb) {
+            self.clog.append({
+                'commitIndex': 0,
+                'term': 0,
+                //No index, so append!
+                'entries': memStream([ { 'term': 2,
+                                         'command': 'command-2-2' } ])
+            }, function (err, entry) {
+                t.equal(2, entry.index);
+                t.deepEqual(self.clog.last(), entry);
+                getAndCompareIndex(2, subcb);
+            });
+        }
+    ];
+    vasync.pipeline({
+        funcs: funcs,
+        arg: {}
+    }, function (err) {
+        if (err) {
+            t.fail(err);
+        }
+        t.done();
+    });
+});
+
+
+test('open with correct state', function (t) {
+    var self = this;
+    var funcs = [
+        function (_, subcb) {
+            self.clog.append({
+                'commitIndex': 0,
+                'term': 0,
+                'entries': memStream([
+                    { 'term': 2, 'command': 'command-1-2' }
+                ])
+            }, function (err, entry) {
+                if (err) {
+                    subcb(err);
+                    return;
+                }
+                t.equal(1, entry.index);
+                t.deepEqual(self.clog.last(), entry);
+                t.equal(2, self.clog.nextIndex);
+                //Stash some stuff for later...
+                _.last = self.clog.last();
+                _.nextIndex = self.clog.nextIndex;
+                subcb();
+            });
+        },
+        //Close the level db
+        function (_, subcb) {
+            self.clog.close(subcb);
+        },
+        //Open a new clog over the old one
+        function (_, subcb) {
+            self.clog = new LevelDbLog({
+                'log': LOG,
+                'location': DB_FILE,
+                'stateMachine': self.stateMachine
+            });
+            self.clog.on('ready', subcb);
+            self.clog.on('error', subcb);
+        },
+        function (_, subcb) {
+            t.deepEqual(_.last, self.clog.last());
+            t.equal(_.nextIndex, self.clog.nextIndex);
+            subcb();
+        }
+    ];
+    vasync.pipeline({
+        funcs: funcs,
+        arg: {}
+    }, function (err) {
+        if (err) {
+            t.fail(err);
+        }
+        t.done();
     });
 });
--- test/leveldbraft/leveldbraft_install_snapshot.test.js
+++ test/leveldbraft/leveldbraft_install_snapshot.test.js
@@ -1,44 +1,51 @@
 // Copyright (c) 2013, Joyent, Inc. All rights reserved.
 
+var assert = require('assert-plus');
 var bunyan = require('bunyan');
+var fs = require('fs');
 var helper = require('../helper.js');
-var memraft = require('../memraft');
+var leveldbraft = require('../leveldbraft');
 var nodeunitPlus = require('nodeunit-plus');
 var vasync = require('vasync');
+
+
 
 // All the actual tests are here...
 var raftInstallSnapshotTests =
     require('../share/raft_install_snapshot_tests.js');
 
 
+
 ///--- Globals
 
+var after = nodeunitPlus.after;
 var before = nodeunitPlus.before;
-var e = helper.e;
-var entryStream = helper.entryStream;
-var test = nodeunitPlus.test;
 var LOG = bunyan.createLogger({
     level: (process.env.LOG_LEVEL || 'fatal'),
-    name: 'memlog-test',
+    name: 'raft-test',
     stream: process.stdout
 });
+var LOW_LEADER_TIMEOUT = 2;
 
 
 
 ///--- Setup/Teardown
 
 before(function (cb) {
+    assert.func(cb, 'cb');
     var self = this;
+
     var peers = [ 'raft-0', 'raft-1' ];
     vasync.forEachParallel({
         'inputs': peers.map(function (p) {
             return ({
                 'log': LOG,
                 'id': p,
-                'peers': [ p ]
+                'peers': [ p ],
+                'dbName': 'raft_install_snapshot_tests_db_' + p
             });
         }),
-        'func': memraft.raft
+        'func': leveldbraft.raft
     }, function (err, res) {
         if (err) {
             return (cb(err));
@@ -55,38 +62,19 @@ before(function (cb) {
 });
 
 
-
-///--- Tests only for the memraft...
-
-test('get snapshot', function (t) {
+after(function (cb) {
     var self = this;
-    var oldRaft = self.oldRaft;
-    var snapshotter = oldRaft.snapshotter;
     vasync.pipeline({
-        funcs: [
-            function (_, subcb) {
-                snapshotter.getLatest(function (err, snapshot) {
-                    if (err) {
-                        return (subcb(err));
-                    }
-                    t.deepEqual({
-                        'peerData': [ 'raft-0' ],
-                        'stateMachineData': {
-                            'commitIndex': 0,
-                            'data': undefined
-                        },
-                        'clogData': [
-                            e(0, 0)
-                        ]
-                    }, snapshot);
-                    subcb();
-                });
+        'funcs': [
+            function closeOldLevelDb(_, subcb) {
+                self.oldRaft.clog.close(subcb);
+            },
+            function closeNewLevelDb(_, subcb) {
+                self.newRaft.clog.close(subcb);
             }
+
         ]
     }, function (err) {
-        if (err) {
-            t.fail(err);
-        }
-        t.done();
+        cb(err);
     });
 });
