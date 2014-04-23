135e8632b1a97346852749dda0abccd0c879b27a
Nate Fitch <nate.fitch@joyent.com>
2014-04-23 09:20:45 -0700

    Tool to dump leveldb to console.

---
+++ bin/dumpdb.js
@@ -0,0 +1,84 @@
+#!/usr/bin/env node
+// Copyright (c) 2014, Joyent, Inc. All rights reserved.
+
+var bunyan = require('bunyan');
+var dashdash = require('dashdash');
+var dbHelpers = require('../lib/leveldb/db_helpers');
+var path = require('path');
+
+
+
+///--- Globals
+
+var LOG = bunyan.createLogger({
+    'level': (process.env.LOG_LEVEL || 'fatal'),
+    'name': 'dumpdb',
+    'stream': process.stdout
+});
+
+var OPTIONS = [
+    {
+        'names': ['help', 'h'],
+        'type': 'bool',
+        'help': 'Print this help and exit.'
+    },
+    {
+        'names': ['location', 'l'],
+        'type': 'string',
+        'help': 'Location of the db.'
+    }
+];
+
+
+
+///--- Main
+
+var dd = dashdash.createParser({ options: OPTIONS });
+try {
+    var _opts = dd.parse(process.argv);
+} catch (e) {
+    console.error('dumpdb: error: %s', e.message);
+    process.exit(1);
+}
+
+function help(msg) {
+    var help = dd.help({ includeEnv: true }).trimRight();
+    if (msg) {
+        console.log('dumpdb: ' + msg);
+    }
+    console.log('WARNING: Do not run while Vera is running.\n'
+                + 'usage: ' + path.basename(process.argv[1]) + ' [OPTIONS]\n'
+                + 'options:\n'
+                + help);
+    process.exit(2);
+}
+
+if (_opts.help) {
+    help();
+}
+
+if (!_opts.location) {
+    help('location is a required parameter');
+}
+
+_opts.log = LOG;
+
+dbHelpers.open(_opts, function (err, db) {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    dbHelpers.dumpDbToConsole(db, function () {
+        db.close(function (err) {
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            }
+            //Exit should exit happily here.
+        });
+    });
+});
+
+process.on('uncaughtException', function (err) {
+    console.log('Caught exception: ' + err);
+});
